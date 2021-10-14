4558cdc664d73cc5828d111308f2d6a64c15326d
Shuo Han <shan50@jhu.edu>
2021-10-13 21:48:55 -0400

    Update setup and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,29 @@
-FROM pytorch/pytorch:1.8.1-cuda10.2-cudnn7-runtime
-RUN apt-get update && apt-get install -y git ffmpeg libsm6 libxext6
-RUN pip install scipy==1.6.3 \
+FROM nvidia/cuda:10.2-runtime-ubuntu18.04
+RUN apt-get update && apt-get install -y git ffmpeg libsm6 libxext6 wget \
+        build-essential openjdk-8-jdk
+ENV PATH="/opt/conda/bin:${PATH}"
+ENV JCC_JDK=/usr/lib/jvm/java-1.8.0-openjdk-$(dpkg --print-architecture)
+WORKDIR /tmp
+RUN wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
+        bash Miniconda3-latest-Linux-x86_64.sh -p /opt/conda -b && \
+        rm Miniconda3-latest-Linux-x86_64.sh
+RUN pip3 install \
+        jcc==3.10 \
+        kornia==0.5.6 \
+        torch==1.9.1 \
+        scipy==1.6.3 \
         nibabel==3.2.1 \
         matplotlib==3.4.2 \
         tqdm==4.51.0 \
         improc3d==0.5.2 \
         opencv-python==4.5.3.56 \
         git+https://gitlab.com/shan-utils/resize.git@0.1.1
+RUN git clone https://github.com/nighres/nighres
+WORKDIR /tmp/nighres
+RUN ./build.sh && pip install . && rm -rf /tmp/nighres
+
 COPY scripts /tmp/scripts
-COPY ct_synth /tmp/ct_synth
+COPY deep_meninges /tmp/deep_meninges
 COPY setup.py README.md /tmp/
 WORKDIR /tmp
 RUN pip install . && rm -rf /tmp/*
--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
 from setuptools import setup
 
-version = '0.2.0'
+version = '0.1.0'
 
 with open('README.md') as readme:
     long_desc = readme.read()
 
 setup(
-    name='ct_synth',
-    description='Mallika\'s CT synthesis.',
+    name='pseudo-3d-meninges',
+    description='Pseudo-3D Meninges Surface Reconstruction',
     author='Shuo Han',
     author_email='shan50@jhu.edu',
     version=version,
-    packages=['ct_synth'],
+    packages=['deep_meninges'],
     license='GPLv3',
-    python_requires='>=3.7.10',
-    scripts=['scripts/test.py', 'scripts/train.py'],
+    python_requires='>=3.9.4',
+    scripts=['scripts/test.py', 'scripts/train.py', 'scripts/tpc.py'],
     long_description=long_desc,
     install_requires=[
         'torch>=1.8.1',
+        'kornia',
         'numpy',
         'nibabel',
+        'matplotlib',
         'opencv-python',
+        'improc3d',
+        'resize@git+https://gitlab.com/shan-utils/resize.git@0.1.1',
         'pytorch-unet@git+https://github.com/shuohan/pytorch-unet.git@0.1.0',
         'ptxl@git+https://gitlab.com/shan-deep-networks/ptxl.git@0.3.1'
     ],
     long_description_content_type='text/markdown',
-    url='https://gitlab.com/iacl/mallika-ct-synth.git',
+    url='https://github.com/shuohan/pseudo-3d-meninges',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
